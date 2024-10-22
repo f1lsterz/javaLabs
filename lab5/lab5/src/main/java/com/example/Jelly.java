4e35223280739cf5c20f7e75cccd9122023886c8
Volodymyr <vovatsyukh@gmail.com>
2024-10-22 21:09:22 +0300

    lab5

---
+++ lab5/lab5/src/main/java/com/example/Jelly.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class Jelly extends Sweet {
+    public Jelly(String name, double weight, double sugarContent) {
+        super(name, weight, sugarContent);
+    }
+}
---
+++ lab5/lab5/src/main/java/com/example/WhiteChocolate.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class WhiteChocolate extends Chocolate {
+    public WhiteChocolate(String name, double weight, double sugarContent, double chocolateContent) {
+        super(name, weight, sugarContent, chocolateContent);
+    }
+}
